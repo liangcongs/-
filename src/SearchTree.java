5bf6a990af01d614703ffcba28307e53f64f104a
liangcong <15290058408@163.com>
2021-05-24 21:30:51 +0800

    线索二叉树

---
+++ src/SearchTree.java
@@ -0,0 +1,46 @@
+public class SearchTree {
+
+   static Node pre;
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+        int lTag;
+        int rTag;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+
+
+    }
+    public static void inthread(Node head) {
+        if (head == null) {
+            return;
+        }
+        inthread(head.left);
+        process(head);
+        inthread(head.right);
+    }
+
+    private static void process(Node head) {
+        if (head.left == null) {
+            head.left=pre;
+            head.lTag=1;
+        }
+        if(pre != null && pre.right == null) {
+            pre.right = head;
+            pre.rTag = 1;
+        }
+        pre = head;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.left = new Node(4);
+        head.right = new Node(7);
+        head.left.left = new Node(8);
+        inthread(head);
+    }
+}
